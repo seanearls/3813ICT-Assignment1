08f61a8e75a7c2642a32d5e96ae5ca7b1190730f
seanearls <sean.earls@griffithuni.edu.au>
2022-09-05 22:16:41 +1000

    Updated app.component.html. Updated app-routing.module.ts

--- assignment1/src/app/app-routing.module.ts
+++ assignment1/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ChatComponent } from './chat/chat.component';
+import { GroupsComponent } from './groups/groups.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'chat', component:ChatComponent},
+  {path:'groups', component:GroupsComponent},
+  {path:'admin', component:AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
