40411e0a72428bb8e2b38542011f77250aff05a7
seanearls <sean.earls@griffithuni.edu.au>
2022-09-05 21:51:30 +1000

    Generated page components.

--- assignment1/src/app/app.module.ts
+++ assignment1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ChatComponent } from './chat/chat.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GroupsComponent,
+    ChatComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
