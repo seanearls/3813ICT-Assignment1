7c43a9ab77498f80179afe6cf622685d474700a9
seanearls <sean.earls@griffithuni.edu.au>
2022-09-05 22:54:34 +1000

    Implemented login.

--- assignment1/src/app/login/login.component.ts
+++ assignment1/src/app/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const serverURL = 'http://localhost:3000';
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private router: Router, private httpClient:HttpClient) {
+
+  }
+
+  username:string="";
+  upwd:string = "";
+
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    let user = {username: this.username, upwd: this.upwd};
+    this.httpClient.post(serverURL + '/api/auth', user, httpOptions).subscribe((data: any) => {
+      if (data.valid){
+        sessionStorage.setItem('username', JSON.stringify(data.username));
+        sessionStorage.setItem('email', JSON.stringify(data.email));
+        sessionStorage.setItem('id', JSON.stringify(data.id));
+        sessionStorage.setItem('role', JSON.stringify(data.id));
+        this.router.navigateByUrl('groups');
+      }
+    });
   }
 
 }
