5b58e5573a7cd850b72bf836b2d2b66b0ae1a373
seanearls <sean.earls@griffithuni.edu.au>
2022-09-05 22:58:25 +1000

    Added io sockets.io.

--- assignment1/server/server.js
+++ assignment1/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); //Import express module
 const app = express(); //Calling top-level express function
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -11,4 +12,4 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '../dist/assignment1')));
 
 require('./sockets.js')(app, io);
-require('./listen.js')(http);
+require('./listen.js')(server);
