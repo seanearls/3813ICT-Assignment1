158836e96bf36a14e45ddc8c32b0268ef2de9a5b
seanearls <sean.earls@griffithuni.edu.au>
2022-10-03 15:10:49 +1000

    Removed redundant code.

--- assignment1/server/routes/onNewUser.js
+++ assignment1/server/routes/onNewUser.js
@@ -1,8 +1,5 @@
 const app = require('express');
 const router = app.Router();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const e = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
@@ -35,31 +32,7 @@ router.post('/', (req, res) => {
             }
         })
         .catch(err => console.log(err));
-    })
-})
-
-//Route for creating new user
-router.post('/', (req, res) => {
-    var newUser;
-    var newUsername = req.body.username;
-    var newEmail = req.body.email;
-    var newRole = req.body.role;
-    var newUpwd = req.body.upwd;
-
-    fs.readFile('users.json', 'utf-8', function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            newUser = JSON.parse(data);
-            newUser.push({'username':newUsername, 'email':newEmail, 'role':newRole});
-            newUser = JSON.stringify(newUser);
-            fs.writeFile('users.json', newUser, 'utf-8', function(err) {
-                if (err) throw err;
-                res.send({'username':newUser, 'registered':true});
-            });
-        }
     });
-
-})
+});
 
 module.exports = router;
